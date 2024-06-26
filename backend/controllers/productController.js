7ebcfd8f5017f2e758edde5c994f2999199ae35b
kishanpaneri <kishan.paneri123@gmail.com>
2024-06-26 21:11:14 +0530

    product api code add

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,11 +4,15 @@ const productController = {
   async index(req, res, next) {
     let products;
     try {
-      products = await Product.find().populate('category');
+      // products = await Product.find().populate({
+      //    path:"categoryId", select: ['title', 'description']
+      // });
+      products = await Product.find().populate("category");
+      // products = await Product.aggregate({});
     } catch (error) {
-      return res.status(404).json({ error: 'Internal Server error' });
+      return res.status(404).json({ error: "Internal Server error" });
     }
-    return res.json(products);
+    return res.json({ status: 200, products });
   },
 
   async store(req, res, next) {
@@ -26,6 +30,60 @@ const productController = {
         .json({ error: "Internal server error.", serverError: error });
     }
     res.status(201).json(cat);
+  },
+
+  async getProductsByCategory(req, res, next) {
+    let cat;
+    try {
+      const id = req.params.id;
+      cat = await Product.find({ categoryId: id }).populate("categoryId");
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Internal server error.", serverError: error });
+    }
+    res.status(200).json(cat);
+  },
+  async search(req, res) {
+    let products = [];
+    try {
+      const searchText = req.query.search;
+      products = await Product.find({
+        title: { $regex: searchText, $options: "i" }, // i makes it case insensitive
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Internal server error.", serverError: error });
+    }
+    res.status(200).json({ products: products });
+  },
+
+  async filterProducts(req, res) {
+    let products = [];
+    try {
+      const { search, minPrice, maxPrice, category } = req.query;
+      let filter = {};
+      if (search) {
+        filter.title = { $regex: search, $options: "i" }; // i make it case-insensitive
+      }
+      if (minPrice) {
+        filter.price = { ...filter.price, $gte: minPrice };
+      }
+      if (maxPrice) {
+        filter.price = { ...filter.price, $lte: maxPrice };
+      }
+      if (category) {
+        filter.category = category;
+      }
+      console.log(filter, "filter");
+      products = await Product.find(filter);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "Internal server error.", serverError: error });
+    }
+    res.status(200).json({ products: products });
   },
 };
 
